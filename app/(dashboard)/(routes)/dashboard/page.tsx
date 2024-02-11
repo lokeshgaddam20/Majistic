2167148cb0e0d08b86eb0635795ee98ee0842c25
Lokesh Gaddam <gaddamlokesh20@gmail.com>
2024-02-11 12:28:43 +0530

    Create Sidebar and Navbar comp

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = () => {
   return (
     <div>
       <p>Dashboard</p>
-      <UserButton afterSignOutUrl="/" />
+      
     </div>
   );
 };

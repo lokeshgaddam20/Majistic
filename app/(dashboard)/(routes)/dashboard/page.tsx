ccef30eabe10e8ff2cd9b6b4489ce442a86b5fbb
Lokesh Gaddam <gaddamlokesh20@gmail.com>
2024-02-11 13:24:27 +0530

    DashBoard Page completed

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -12,7 +12,7 @@ export default function HomePage() {
   const router = useRouter();
 
   return (
-    <div>
+    <div> 
       <div className="mb-8 space-y-4">
         <h2 className="text-2xl md:text-4xl font-bold text-center">
           Explore the power of AI

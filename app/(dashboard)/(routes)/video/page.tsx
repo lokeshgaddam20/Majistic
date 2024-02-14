56fbf1482a8132bdf9f728dd4d41e5f209f2e374
Lokesh Gaddam <gaddamlokesh20@gmail.com>
2024-02-14 19:16:31 +0530

    Video AI working

--- app/(dashboard)/(routes)/video/page.tsx
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -1,31 +1,34 @@
 "use client";
 
 import * as z from "zod";
-import { Heading } from "@/components/heading";
-import { Video } from "lucide-react";
-import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import { VideoIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { Heading } from "@/components/heading";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Loader } from "@/components/loader";
+import { Empty } from "@/components/ui/empty";
+// import { useProModal } from "@/hooks/use-pro-modal";
 
 import { formSchema } from "./constants";
-import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
-import { useState } from "react";
-import { toast } from "react-hot-toast";
-import { Empty } from "@/components/ui/empty";
-import { Loader } from "@/components/loader";
 
 const VideoPage = () => {
   const router = useRouter();
+  // const proModal = useProModal();
   const [video, setVideo] = useState<string>();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       prompt: "",
-    },
+    }
   });
 
   const isLoading = form.formState.isSubmitting;
@@ -34,92 +37,83 @@ const VideoPage = () => {
     try {
       setVideo(undefined);
 
-      const response = await axios.post("/api/video");
+      const response = await axios.post('/api/video', values);
 
       setVideo(response.data[0]);
       form.reset();
     } catch (error: any) {
       if (error?.response?.status === 403) {
         // proModal.onOpen();
-        console.log("if, catch block ; conversation:page.tsx");
       } else {
         toast.error("Something went wrong.");
       }
     } finally {
       router.refresh();
     }
-  };
+  }
 
-  return (
+  return ( 
     <div>
       <Heading
         title="Video Generation"
-        description="Find the worst short videos that you don't want."
-        icon={Video}
-        iconColor="text-orange-400"
-        bgColor="bg-orange-400/10"
+        description="Turn your prompt into video."
+        icon={VideoIcon}
+        iconColor="text-orange-700"
+        bgColor="bg-orange-700/10"
       />
-
       <div className="px-4 lg:px-8">
-        <div>
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className="rounded-lg 
-                border 
-                w-full 
-                p-4 
-                px-3 
-                md:px-6 
-                focus-within:shadow-sm
-                grid
-                grid-cols-12
-                gap-2"
-            >
-              <FormField
-                name="prompt"
-                render={({ field }) => (
-                  <FormItem className="col-span-12 lg:col-span-10">
-                    <FormControl className="m-0 p-0">
-                      <Input
-                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
-                        disabled={isLoading}
-                        placeholder="Write to get your fav videos ever heard..."
-                        {...field}
-                      />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
-              <Button
-                className="col-span-12 lg:col-span-2 w-full"
-                type="submit"
-                disabled={isLoading}
-                size="icon"
-              >
-                Generate
-              </Button>
-            </form>
-          </Form>
-        </div>
-        <div className="space-y-4 mt-4">
-          {isLoading && (
-            <div className="p-20">
-              <Loader />
-            </div>
-          )}
-          {!video && !isLoading && (
-            <Empty label="No video generated." />
-          )}
-          {video && (
+        <Form {...form}>
+          <form 
+            onSubmit={form.handleSubmit(onSubmit)} 
+            className="
+              rounded-lg 
+              border 
+              w-full 
+              p-4 
+              px-3 
+              md:px-6 
+              focus-within:shadow-sm
+              grid
+              grid-cols-12
+              gap-2
+            "
+          >
+            <FormField
+              name="prompt"
+              render={({ field }) => (
+                <FormItem className="col-span-12 lg:col-span-10">
+                  <FormControl className="m-0 p-0">
+                    <Input
+                      className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                      disabled={isLoading} 
+                      placeholder="Clown fish swimming in a coral reef" 
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button className="col-span-12 lg:col-span-2 w-full" type="submit" disabled={isLoading} size="icon">
+              Generate
+            </Button>
+          </form>
+        </Form>
+        {isLoading && (
+          <div className="p-20">
+            <Loader />
+          </div>
+        )}
+        {!video && !isLoading && (
+          <Empty label="No video files generated." />
+        )}
+        {video && (
           <video controls className="w-full aspect-video mt-8 rounded-lg border bg-black">
             <source src={video} />
           </video>
         )}
-        </div>
       </div>
     </div>
-  );
-};
-
+   );
+}
+ 
 export default VideoPage;

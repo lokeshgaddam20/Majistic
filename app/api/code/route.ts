1af0f3208306e3a9f81ea7318bcdce088e7365ab
Lokesh Gaddam <gaddamlokesh20@gmail.com>
2024-02-17 21:58:56 +0530

    CheckApiLimit added to all apis route.ts

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -2,6 +2,8 @@ import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
 
+import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -33,17 +35,18 @@ export async function POST(
         return new NextResponse("Messages are required", { status: 400 });
       }
   
-    //   const freeTrial = await checkApiLimit();
+      const freeTrial = await checkApiLimit();
     //   const isPro = await checkSubscription();
   
-    //   if (!freeTrial && !isPro) {
-    //     return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
-    //   }
+      if (!freeTrial) {
+        return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
+      }
   
       const response = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [instructionMessage, ...messages]
       });
+      await incrementApiLimit();
   
     //   if (!isPro) {
     //     await incrementApiLimit();
--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import { Configuration, OpenAIApi } from "openai";
 
+import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -13,42 +15,43 @@ export async function POST(req: Request) {
     const { userId } = auth();
     const body = await req.json();
     const { prompt, amount = 1, resolution = "512x512" } = body;
-
+    
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
-
+    
     if (!configuration.apiKey) {
       return new NextResponse("OpenAI API Key not configured.", {
         status: 500,
       });
     }
-
+    
     if (!prompt) {
       return new NextResponse("Prompt is required", { status: 400 });
     }
-
+    
     if (!amount) {
       return new NextResponse("Amount is required", { status: 400 });
     }
-
+    
     if (!resolution) {
       return new NextResponse("Resolution is required", { status: 400 });
     }
-
-    //   const freeTrial = await checkApiLimit();
+    
+      const freeTrial = await checkApiLimit();
     //   const isPro = await checkSubscription();
-
-    //   if (!freeTrial && !isPro) {
-    //     return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
-    //   }
-
-    const response = await openai.createImage({
-      prompt: prompt,
-      n: parseInt(amount, 10),
-      size: resolution,
-    });
-
+    
+    if (!freeTrial) {
+          return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
+        }
+    
+      const response = await openai.createImage({
+        prompt: prompt,
+        n: parseInt(amount, 10),
+        size: resolution,
+      });
+      
+     await incrementApiLimit();
     //   if (!isPro) {
     //     await incrementApiLimit();
     //   }

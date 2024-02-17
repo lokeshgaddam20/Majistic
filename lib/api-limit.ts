2b26380c4774be3492e7ddeab793caa1c4037968
Lokesh Gaddam <gaddamlokesh20@gmail.com>
2024-02-17 18:35:10 +0530

    CheckApiLimti Update

--- lib/api-limit.ts
+++ lib/api-limit.ts
@@ -25,3 +25,20 @@ export const incrementApiLimit = async () => {
     });
   }
 };
+export const checkApiLimit = async () => {
+    const { userId } = auth();
+  
+    if (!userId) {
+      return false;
+    }
+  
+    const userApiLimit = await prismadb.userApiLimit.findUnique({
+      where: { userId: userId },
+    });
+  
+    if (!userApiLimit || userApiLimit.count < MAX_FREE_COUNTS) {
+      return true;
+    } else {
+      return false;
+    }
+  };

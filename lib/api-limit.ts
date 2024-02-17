c5b290ada53469f1f71385f50024077cfe092889
Lokesh Gaddam <gaddamlokesh20@gmail.com>
2024-02-17 18:33:50 +0530

    Cretae api-limit and incrementAPiLimit

---
+++ lib/api-limit.ts
@@ -0,0 +1,27 @@
+import { auth } from "@clerk/nextjs";
+
+import prismadb from "@/lib/prismadb";
+import { MAX_FREE_COUNTS } from "@/constants";
+
+export const incrementApiLimit = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return;
+  }
+
+  const userApiLimit = await prismadb.userApiLimit.findUnique({
+    where: { userId: userId },
+  });
+
+  if (userApiLimit) {
+    await prismadb.userApiLimit.update({
+      where: { userId: userId },
+      data: { count: userApiLimit.count + 1 },
+    });
+  } else {
+    await prismadb.userApiLimit.create({
+      data: { userId: userId, count: 1 },
+    });
+  }
+};

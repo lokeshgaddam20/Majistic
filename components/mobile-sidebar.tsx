ae93c6134608b8961d3ba78f2d405591007d6c02
Lokesh Gaddam <gaddamlokesh20@gmail.com>
2024-02-18 10:19:15 +0530

    Mobile-sidebar Update working

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -7,13 +7,13 @@ import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
 
-const MobileSidebar = () =>{
-//   apiLimitCount = 0,
-//   isPro = false
-// }: {
-//   apiLimitCount: number;
-//   isPro: boolean;
-// }) => {
+const MobileSidebar = ({
+  apiLimitCount = 0,
+  // isPro = false
+}: {
+  apiLimitCount: number;
+  // isPro: boolean;
+}) => {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
@@ -31,8 +31,8 @@ const MobileSidebar = () =>{
           <Menu />
         </Button>
       </SheetTrigger>
-      <SheetContent side="left" className="p-0">
-        <Sidebar />
+      <SheetContent side="left" className="p-0 w-full md:w-auto">
+        <Sidebar apiLimitCount={apiLimitCount}/>
       </SheetContent>
     </Sheet>
   );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,15 @@
-import { Menu } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
+import MobileSidebar from "@/components/mobile-sidebar";
+import { getApiLimitCount } from "@/lib/api-limit";
+// import { checkSubscription } from "@/lib/subscription";
 
-const Navbar = () => {
+const Navbar = async() => {
+  const apiLimitCount = await getApiLimitCount();
+  // const isPro = await checkSubscription();
+
   return (
     <div className="flex items-center p-4">
-      <Button variant={"ghost"} size="icon" className="md:hidden">
-        <Menu />
-      </Button>
+      <MobileSidebar apiLimitCount={apiLimitCount}/>
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
       </div>

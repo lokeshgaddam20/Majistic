60b2f379909c7f9b83ecede35b4ce6245815103f
Lokesh Gaddam <gaddamlokesh20@gmail.com>
2024-02-18 09:43:01 +0530

    Progress bar counter and btn-primary change

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -7,6 +7,7 @@ import { Montserrat } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
+import { FreeCounter } from "@/components/free-counter";
 
 const montserrat = Montserrat({
   weight: ["400", "500", "600", "700"],
@@ -57,7 +58,14 @@ const routes = [
   },
 ];
 
-const Sidebar = () => {
+interface SidebarProps {
+  apiLimitCount: number;
+}
+
+
+const Sidebar = ({
+  apiLimitCount = 0,
+}: SidebarProps) => {
   const pathname  = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
@@ -87,6 +95,9 @@ const Sidebar = () => {
           ))}
         </div>
       </div>
+      <FreeCounter
+        apiLimitCount={apiLimitCount}
+      />
     </div>
   );
 };
